0fe48bd37e0027862b0967123cb9fc7045a4d99d
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2026-05-05 16:04:28 +0200

    Update navigation links to use PHP files

--- Pages/Team.php
+++ Pages/Team.php
@@ -9,18 +9,18 @@
 <body>
     <nav class="navbar">
         <div class="logo">
-            <a href="../index.html"><img src="../images/hd-media-logo.svg" alt="HD Media Logo"></a>
+            <a href="../index.php"><img src="../images/hd-media-logo.svg" alt="HD Media Logo"></a>
         </div>
         <ul class="nav-links">
-            <li><a href="../index.html">Home</a></li>
-            <li><a href="Services.html">Services</a></li>
-            <li><a href="Portfolio.html">Portfolio</a></li>
-            <li><a href="Pricing.html">Pricing</a></li>
-            <li><a href="Team.html" class="active">Team</a></li>
-            <li><a href="Testimonials.html">Testimonials</a></li>
-            <li><a href="Bookings.html">Bookings</a></li>
-            <li><a href="About.html">About</a></li>
-            <li><a href="Contact.html">Contact</a></li>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="Services.php">Services</a></li>
+            <li><a href="Portfolio.php">Portfolio</a></li>
+            <li><a href="Pricing.php">Pricing</a></li>
+            <li><a href="Team.php" class="active">Team</a></li>
+            <li><a href="Testimonials.php">Testimonials</a></li>
+            <li><a href="Bookings.php">Bookings</a></li>
+            <li><a href="About.php">About</a></li>
+            <li><a href="Contact.php">Contact</a></li>
         </ul>
     </nav>
 
